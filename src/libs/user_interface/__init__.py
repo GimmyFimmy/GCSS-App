7a455397966eba8de222bc2922fe5830146b38ff
Gimmy_Fimmy <k.pershin2008@gmail.com>
2025-02-16 02:23:43 +0300

    gesture_name.py added devices_settings.py added

--- src/libs/user_interface/__init__.py
+++ src/libs/user_interface/__init__.py
@@ -1,4 +1,4 @@
 from src.libs.user_interface.windows.menu import Menu
 from src.libs.user_interface.windows.gesture_name import GestureName
-from src.libs.user_interface.windows.devices_manager import DevicesManager
-from src.libs.user_interface.windows.gestures_manager import GesturesManager
+from src.libs.user_interface.windows.devices_editor import DevicesEditor
+from src.libs.user_interface.windows.gestures_editor import GesturesEditor
