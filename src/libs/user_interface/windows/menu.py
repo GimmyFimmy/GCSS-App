30a7201ceab6b07fb2acb5fd2de290645c94ffa6
Gimmy_Fimmy <k.pershin2008@gmail.com>
2025-02-16 02:27:21 +0300

    refreshed ui

--- src/libs/user_interface/windows/menu.py
+++ src/libs/user_interface/windows/menu.py
@@ -8,15 +8,17 @@ from src.constants import (
 )
 
 class Menu:
-    def __init__(self, on_button_pressed):
-        self.on_button_pressed = on_button_pressed
+    def __init__(self, on_button_pressed, on_display_info_pressed):
+        self.callback = on_button_pressed
+        self.display = on_display_info_pressed
 
         self.time = None
         self.date = None
+        self.weather = None
 
     def create(self, *args):
-        self.window = Create.window(600, 360)
-        self.canvas = Create.canvas(self.window, 360, 600)
+        self.window = Create.window(1000, 600)
+        self.canvas = Create.canvas(self.window, 600, 1000)
 
         self.image_image_1 = Create.image(0, "image_1")
         self.image_image_2 = Create.image(0, "image_2")
@@ -24,74 +26,82 @@ class Menu:
         self.button_image_2 = Create.image(0, "button_2")
         self.button_image_3 = Create.image(0, "button_3")
 
-        def update_time():
+        def update():
             current_time = time.strftime('%H:%M')
 
             date = datetime.date.today()
             weekday = WEEKDAYS[datetime.date.weekday(date)]
 
             self.canvas.itemconfig(tagOrId=self.time, text=current_time)
-            self.canvas.itemconfig(tagOrId=self.date, text=f'{weekday}, {date.day}')
+            self.canvas.itemconfig(tagOrId=self.date, text=f'{weekday}.\n{date.day}')
 
-            self.window.after(UPDATE_DELAY, update_time)
+            self.window.after(UPDATE_DELAY, update)
 
         Create.frame(
             self.canvas,
+            500.0,
             300.0,
-            180.0,
             self.image_image_1
         )
 
-        self.time = Create.label(
-            self.canvas,
-            240.0,
-            55.0,
-            "",
-            48
-        )
-
-        self.date = Create.label(
-            self.canvas,
-            255.0,
-            115.0,
-            "",
-            14
-        )
-
         Create.frame(
             self.canvas,
-            300.0,
-            320.0,
+            335.0,
+            520.0,
             self.image_image_2
         )
 
         Create.button(
             self.button_image_1,
-            355.0,
-            300.0,
-            40.0,
-            40.0,
-            lambda: self.on_button_pressed(0),
+            475.0,
+            475.0,
+            90.0,
+            90.0,
+            lambda: self.callback(1, False),
         )
 
         Create.button(
             self.button_image_2,
-            280.0,
-            300.0,
-            40.0,
-            40.0,
-            lambda: self.on_button_pressed(2, False)
+            585.0,
+            475.0,
+            90.0,
+            90.0,
+            lambda: self.callback(2)
         )
 
         Create.button(
             self.button_image_3,
-            205.0,
-            300.0,
-            40.0,
-            40.0,
-            lambda: self.on_button_pressed(1, False)
+            695.0,
+            475.0,
+            90.0,
+            90.0,
+            lambda: self.display()
         )
 
-        update_time()
+        self.time = Create.label(
+            self.canvas,
+            313.0,
+            63.0,
+            '',
+            128
+        )
+
+        self.date = Create.label(
+            self.canvas,
+            253.0,
+            486.0,
+            '',
+            28
+        )
+
+        self.weather = Create.label(
+            self.canvas,
+            364.0,
+            503.0,
+            '~~°C',
+            28
+        )
+
+        update()
 
         self.window.mainloop()
